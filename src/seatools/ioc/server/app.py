cc8dca8d32d3eef7584f7dc44c76c84501e6a21b
dragons96 <jinqunlong@black-unique.com>
2024-12-17 18:15:30 +0800

    update: 规范化asgi app

--- src/seatools/ioc/server/app.py
+++ src/seatools/ioc/server/app.py
@@ -2,7 +2,7 @@ from seatools.ioc import Autowired
 from fastapi import FastAPI
 
 
-def app():
+def asgi_app():
     """server app factory."""
     # Get FastAPI instance from ioc_bean()
     return Autowired(cls=FastAPI).ioc_bean()
